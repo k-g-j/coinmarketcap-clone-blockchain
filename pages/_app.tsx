61d8c934f70645513a47a9cb7d753f93ad257c7d
Kate Johnson <johnson.kat.g@gmail.com>
2022-05-09 02:48:32 -0400

    main ui complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MoralisProvider } from 'react-moralis'
+import {CoinMarketProvider} from '../context/context'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_SERVER}
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+    >
+      <CoinMarketProvider>
+        <Component {...pageProps} />
+      </CoinMarketProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from 'next'
 import Header from '../components/Header'
+import Trending from '../components/Trending'
+import CMCtable from '../components/cmc-table/CMCtable'
 
 const Home: NextPage = () => {
   return (
     <div className='min-h-screen'>
       <Header />
       <div className='mt-10' />
-      {/* <Trending /> */}
+      <Trending />
       <div className='mt-20' />
-      {/* <CMCtable /> */}
+      <CMCtable />
     </div>
   )
 }
